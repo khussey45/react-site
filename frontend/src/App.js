22a0143100236a644911d48c51b5f7feb2fbf31c
Kieren Hussey <kierenhussey@Kierens-MacBook-Pro.local>
2024-09-02 14:23:25 -0400

    Nav Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,16 +10,19 @@ function App() {
 
   return (
     <nav className="navbar">
-      <ul className="navbar-list">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-      <div className="hamburger" onClick={toggleMenu}>
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
+      <div className="navbar-title">Kieren Hussey</div>
+      <div className="navbar-right">
+        <ul className="navbar-list">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <div className="hamburger" onClick={toggleMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
       </div>
       <ul className={`dropdown-menu ${isOpen ? 'show' : ''}`}>
         <li><a href="#home">Home</a></li>
